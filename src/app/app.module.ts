ace047394a66060b7975de9eb760357a157fd124
Anthony Nahas <anthony.na@hotmail.de>
2019-11-30 22:14:59 +0100

    fix(lib): added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {MatLoadersModule} from '@angular-material-extensions/loaders';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    MatLoadersModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    MatLoadersModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
